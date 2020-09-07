5abbfe5486e7f02f14c93206662dd5e529db0224
Echono <wonger1324@gmail.com>
2020-09-07 11:01:23 +0200

    Update Program.cs

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace Hello {
     class Program {
+
+        private static List<string> list = new List<string>();
+
         static void Main(string[] args) {
             Console.WriteLine("Hej, Casper was here");
             Console.WriteLine("LOL DUDE Same");
+<<<<<<< Updated upstream
             Console.WriteLine("Gustav er en dejlig drengn");
             Console.WriteLine("1337 haX0r");
+=======
+            Console.WriteLine("Gustav er en dejlig dreng");
+            
+            foreach(var v in list) {
+                Console.WriteLine(v);
+            }
+
+        }
+
+        private void insertStrings() {
+            list.Add("Casper was here");
+>>>>>>> Stashed changes
         }
     }
 }
