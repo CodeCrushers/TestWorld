ad74f4223925beb926dcda62d78fcab54fea8bb5
Echono <wonger1324@gmail.com>
2020-09-07 10:27:42 +0200

    Casper was here

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Hello {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hej, Casper was here");
         }
     }
 }
