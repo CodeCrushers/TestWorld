806f04778623f230ff312f332bb998cca66c939c
Alyskys <Gunstavoflol@gmail.com>
2020-09-07 11:00:25 +0200

    Tal ting

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace Hello {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hej, Casper was here");
-            Console.WriteLine("LOL DUDE Same");
-            Console.WriteLine("Gustav er en dejlig drengn");
+            int tal = 10;
+            int[] flereTal = new int[10];
+            while (tal > 0)
+            {
+                tal--;
+                flereTal[tal] = tal;
+                Console.WriteLine(tal);
+            }
         }
     }
 }
